d761b5e44c6a1e6cd61146f42d5543f1ad15d093
Lucas Eggers <l.eggers91@gmail.com>
2014-01-01 14:31:36 -0800

    added more graph functions

--- Data Structures/graph.py
+++ Data Structures/graph.py
@@ -5,17 +5,41 @@
 
 class Graph:
     """Graph data structure"""
-    def __init__(self, graphString):
+    def __init__(self, graphString = ''):
         self.vertices = []
         self.edges = []
+
+    def add_vertex(self, name):
+        vertex = Vertex(name)
+        self.vertices.append(vertex)
+        return vertex
+
+    def connect_vertices(self, v1, v2):
+        edge = Edge(v1, v2)
+        self.edges.append(edge)
+        return edge
 
 class Edge:
     """A graph edge"""
     def __init__(self, v1, v2):
+        v1.edges.append(self)
+        v2.edges.append(self)
         self.v1 = v1
         self.v2 = v2
 
+    def other_end(self, v):
+        if v == self.v1:
+            return self.v2
+        else:
+            return self.v1
+
+
 class Vertex:
     """A graph vertex"""
-    def __init__(self, name):
+    def __init__(self, name, weight = 1):
         self.name = name
+        self.edges = []
+        self.weight = weight
+
+    def add_edge(self, edge):
+        self.edges.append(edge)
