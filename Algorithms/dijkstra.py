7769cc7f63344db5b635af8e2dae9fbee27eff84
Lucas Eggers <l.eggers91@gmail.com>
2014-01-04 16:38:51 -0800

    dijkstra class scaffold

---
+++ Algorithms/dijkstra.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# Author: Lucas Eggers
+
+# Class that implements Dijkstra's graph search algorithm
+
+class Dijkstra():
+    """Class that implements Dijstra's graph serach algorithm"""
+    def __init__(self, graph):
+        self.graph = graph
+
+    def solve(self, graph = None):
+        """Returns an array of graph, edge, graph, edge... that
+        makes the path from start node to end."""
+        pass
+
+    def solution_string(self):
+        """Prints the solution as a string."""
+        pass
