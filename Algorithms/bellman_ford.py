c4eee5cb35abe9390d023f2834c92c1fc85d62f7
Lucas Eggers <l.eggers91@gmail.com>
2014-01-06 21:13:20 -0800

    allowed bellman_ford to take no arguments

--- Algorithms/bellman_ford.py
+++ Algorithms/bellman_ford.py
@@ -9,7 +9,7 @@ from generic_solver import *
 class Bellman_Ford(GenericSolver):
     """Class that implements the Bellman-Ford graph search algorithm.
     Useful for dealing with negative edge weights."""
-    def __init__(self, graph):
+    def __init__(self, graph = None):
         super(Bellman_Ford, self).__init__(graph)
 
     def solve(self, initial, graph):
