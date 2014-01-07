d4e83f3985d88f338b6b3fc107c3608a507b8dda
Lucas Eggers <l.eggers91@gmail.com>
2014-01-06 17:44:57 -0800

    added top-level importer for modules

---
+++ importer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Author: Lucas Eggers
+
+# Imports everything in subdirectories
+
+import os, sys
+
+sys.path.append(os.getcwd() + '/Algorithms')
+sys.path.append(os.getcwd() + '/Data Structures')
