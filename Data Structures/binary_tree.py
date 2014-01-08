b4ce159dac321ad84ea8de4706e6f8b325e0c959
Lucas Eggers <l.eggers91@gmail.com>
2014-01-07 18:42:46 -0800

    rewrote insert a bit

--- Data Structures/binary_tree.py
+++ Data Structures/binary_tree.py
@@ -4,31 +4,45 @@
 # A binary tree data structure
 
 class BinaryTree(object):
-    """A binary tree data structure"""
+    """A binary tree data structure. A naive implementation.
+    Not a balanced tree or even a search tree. Just an arbitrary tree."""
     def __init__(self):
         super(BinaryTree, self).__init__()
         self.root = None
 
     def is_empty(self):
+        """Returns whether or not the tree is empty"""
         return self.root == None
 
     def insert(self, value, parent = None, child = None):
-        if parent == None and not self.root == None:
-            raise Exception("No parent specified for a non-root node")
-        if not child.is_a_child_of(parent):
-            raise Exception("Inserting between two non-connected nodes")
-
+        """Inserts a new node into the tree. Can be inserted between two nodes
+        by specifying the parent and child nodes. Can be added as leaf by 
+        only specifying parent (if that parent does not already have two children!"""
         new_node = TreeNode(value, parent)
 
-        if self.root == None:
+        # adding or replacing a root node
+        if self.is_empty():
             self.root = new_node
+        elif child == self.root:
+            self.root = new_node
+            new_node.add_child(child)
 
-        if child.is_left_child_of(parent):
-            parent.left_child = new_node
-            new_node.left_child = child
-        elif child.is_right_child_of(parent):
-            parent.right_child = new_node
-            new_node.right_child = child
+        if parent:
+            if child:
+                if not child.is_a_child_of(parent):
+                    raise Exception("Inserting between two non-connected nodes")
+                elif child.is_left_child_of(parent):
+                    parent.left_child = new_node
+                    new_node.left_child = child
+                elif child.is_right_child_of(parent):
+                    parent.right_child = new_node
+                    new_node.right_child = child
+                child.parent = new_node
+            else:
+                parent.add_child(new_node)
+        else:
+            if not new_node == self.root:
+                raise Exception("Cannot add a non-root node without a parent")
 
     def remove(self, node):
         parent = node.parent
@@ -67,8 +81,6 @@ class BinaryTree(object):
             raise Exception("Node is not a child of its supposed parent. Uh oh")
 
 
-
-
 class TreeNode(object):
     """A node in a binary tree"""
     def __init__(self, value, parent, left_child = None, right_child = None):
@@ -93,8 +105,10 @@ class TreeNode(object):
     def add_child(self, node):
         if self.left_child == None:
             self.left_child = node
+            node.parent = self
         elif self.right_child == None:
             self.right_child = node
+            node.parent = self
         else:
             raise Exception("Adding a child to a node with two children")
 
