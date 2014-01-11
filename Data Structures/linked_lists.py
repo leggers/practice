43ea59952f3c2f74fd0b073ff94fe68df7c408a7
Lucas Eggers <l.eggers91@gmail.com>
2014-01-11 15:42:20 -0800

    singly linked list first draft

--- Data Structures/linked_lists.py
+++ Data Structures/linked_lists.py
@@ -3,11 +3,40 @@
 
 # A linked list data structure
 
-class SinglyLinkedList(object):
+class AbstractList(object):
+    """Class that implements shared list methods"""
+    def __init__(self):
+        super(AbstractList, self).__init__()
+        self.first = None
+
+
+class SinglyLinkedList(AbstractList):
     """Singly linked list data structure"""
     def __init__(self):
         super(SinglyLinkedList, self).__init__()
-        self.first = None
+
+    def insert_at_beginning(self, value):
+        new_node = SinglyLinkedNode(value, self.first)
+        self.first = new_node
+        return new_node
+
+    def insert_after(self, node, value):
+        new_node = SinglyLinkedNode(value, node.next)
+        node.next = new_node
+        return new_node
+
+    def remove_beginning(self):
+        old_beginning = self.first
+        self.first = old_beginning.next
+        old_beginning.next = None
+        return old_beginning.value
+
+    def remove_after(self, node):
+        to_remove = node.next
+        node.next = to_remove.next
+        to_remove.next = None
+        return to_remove.value
+
 
 class SinglyLinkedNode(object):
     """Singly-linked list node"""
@@ -18,11 +47,10 @@ class SinglyLinkedNode(object):
 
 
 
-class DoublyLinkedList(object):
+class DoublyLinkedList(AbstractList):
     """Doubly linked list data structure"""
     def __init__(self):
         super(DoublyLinkedList, self).__init__()
-        self.first = None
         
 
 class DoublyLinkedNode(object):
